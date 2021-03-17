1f22b11ca71635c641f110806a3b4d87fc718800
LuizCorrea-Dev <correa.luizbatista@gmail.com>
2021-03-17 18:59:28 -0300

    teste de revalidando dados da pagina

--- pages/tempo.js
+++ pages/tempo.js
@@ -6,7 +6,7 @@ function Tempo (props) {
   return(
     <div>
       <div>{dynamicDateString} (dinâmico)</div>
-      <div>{props.staticDateString} (estático)</div>
+      <div>{props.staticDateString} (estático: revalidado)</div>
     </div>
   )
 }
@@ -19,7 +19,8 @@ export function getStaticProps() { // estes dados não vai para o front-end
   return{
     props: { //este props será injeto do componente TEMPO da página
       staticDateString
-    }
+    },
+    revalidate:1 // tempo em segudos para revalidar os dados da pagina. está página carrega com sucesso .
   }
 }
 
